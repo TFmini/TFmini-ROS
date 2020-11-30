6e0fd339f55f610421059f55b34a259254a03f2c
Bhuvanchandra DV <bhuvanchandra.dv@rapyuta-robotics.com>
2020-11-30 08:43:14 +0000

    update the params for TFMini Plus. FIXME: identify the TFMini plus at runtime and set the parameters accordingly

--- tfmini_ros/src/TFmini_ros_node.cpp
+++ tfmini_ros/src/TFmini_ros_node.cpp
@@ -1,7 +1,6 @@
 #include <TFmini.h>
 
-int main(int argc, char **argv)
-{
+int main(int argc, char **argv) {
   ros::init(argc, argv, "tfmini_ros_node");
   ros::NodeHandle nh("~");
   std::string id = "TFmini";
@@ -16,30 +15,24 @@ int main(int argc, char **argv)
   ros::Publisher pub_range = nh.advertise<sensor_msgs::Range>(id, 1000, true);
   sensor_msgs::Range TFmini_range;
   TFmini_range.radiation_type = sensor_msgs::Range::INFRARED;
-  TFmini_range.field_of_view = 0.04;
-  TFmini_range.min_range = 0.3;
+  TFmini_range.field_of_view = 0.0628319;
+  TFmini_range.min_range = 0.1;
   TFmini_range.max_range = 12;
   TFmini_range.header.frame_id = id;
   float dist = 0;
   ROS_INFO_STREAM("Start processing ...");
 
-  while(ros::master::check() && ros::ok())
-  {
+  while (ros::master::check() && ros::ok()) {
     ros::spinOnce();
     dist = tfmini_obj->getDist();
-    if(dist > 0 && dist < TFmini_range.max_range)
-    {
+    if (dist > 0 && dist < TFmini_range.max_range) {
       TFmini_range.range = dist;
       TFmini_range.header.stamp = ros::Time::now();
       pub_range.publish(TFmini_range);
-    }
-    else if(dist == -1.0)
-    {
+    } else if (dist == -1.0) {
       ROS_ERROR_STREAM("Failed to read data. TFmini ros node stopped!");
       break;
-    }
-    else if(dist == 0.0)
-    {
+    } else if (dist == 0.0) {
       ROS_ERROR_STREAM("Data validation error!");
     }
   }
